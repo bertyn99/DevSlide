a2b3a1cada8b4f39fa74263476752e1262d1a825
Bertyn Boulikou <bboulikou@gmail.com>
2022-03-01 13:23:33 +0100

    maj architecture du build electron

---
+++ src/electron/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  if (process.env.NODE_ENV !== "production") {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadURL("file:../../dist/index.html");
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
