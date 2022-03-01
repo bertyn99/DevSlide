81a763ae93ccf63dca6f47a7de464984bddf6005
AyaHaddad <aya.haddad@ynov.com>
2022-03-01 16:58:27 +0100

    add css home + components createSlide/ListPres

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -9,5 +8,5 @@ import HelloWorld from "./components/HelloWorld.vue";
 </template>
 
 <style lang="scss">
-@import "./assets/scss/main.scss";
+@import "./assets/style/main.scss";
 </style>
---
+++ src/components/CreatePres.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="createPres">
+    <div>
+      <h2 class="createPres__title">
+        Create a presentation
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="createPres__icon"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
+          />
+        </svg>
+      </h2>
+    </div>
+    <div>
+      <button class="createPres__button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="iconAddButton"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 4v16m8-8H4"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
