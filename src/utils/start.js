192393aed4612ef44bfd5300519842fb8755a1bc
bertyn99 <bboulikou@gmail.com>
2022-03-11 16:12:49 +0100

    fix npm on windows

--- src/utils/start.js
+++ src/utils/start.js
@@ -1,11 +1,17 @@
-import { spawn } from "child_process";
+const { spawn } = require("child_process");
+const os = require("os");
 
-const vite = spawn("npm", ["run", "dev"]);
+let cmd = "npm";
+if (os.platform() === "win32") {
+  cmd = "npm.cmd";
+}
+
+const vite = spawn(cmd, ["run", "dev"]);
 let electron;
 vite.stdout.on("data", (data) => {
   const str = data.toString();
   if (str.includes("ready in ")) {
-    electron = spawn("npm", ["run", "electron:start"]);
+    electron = spawn(cmd, ["run", "electron:start"]);
     electron.stdout.pipe(process.stdout);
     electron.stderr.pipe(process.stderr);
   }
