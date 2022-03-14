b99eb3eefb120d15d3c7100a5377dea2c76a86c0
AyaHaddad <aya.haddad@ynov.com>
2022-03-14 14:54:23 +0100

    fix button upload + css

--- src/components/ListPres.vue
+++ src/components/ListPres.vue
@@ -1,31 +1,38 @@
 <template>
-  <div class="list-container">
-    <div class="prez-items prez-add">
-      <input type="file" name="codeprez" id="" title="" />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="icon"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-        />
-      </svg>
-      <button type="button" class="">Upload</button>
+  <div class="allPrez">
+    <div class="prez-add">
+      <h2>View your presentations</h2>
+      <button @click="uploadPrez">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="uploadIcon"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+          />
+        </svg>
+      </button>
+    </div>
+    <div class="list-container">
+      <div class="prez-items">
+        <h1>pres</h1>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 /* const { dialog } = require("electron"); */
-window.api.sayHelloInBrowser();
-console.log(window.api);
+
 function loadPrezInMemory() {}
 
-function uploadPrez() {}
+function uploadPrez() {
+  window.api.sayHelloInBrowser();
+}
 </script>
