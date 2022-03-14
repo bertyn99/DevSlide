b82a8179f81d0169875d089f35bb8d5be502d005
bertyn99 <bboulikou@gmail.com>
2022-03-14 01:07:26 +0100

    ajout bouton upload

---
+++ src/components/ListPres.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list-container">
+    <div class="prez-items prez-add">
+      <input type="file" name="codeprez" id="" title="" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="icon"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+        />
+      </svg>
+      <button type="button" class="">Upload</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/* const { dialog } = require("electron"); */
+window.api.sayHelloInBrowser();
+console.log(window.api);
+function loadPrezInMemory() {}
+
+function uploadPrez() {}
+</script>
