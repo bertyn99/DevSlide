a7ea3226ffed57bedaedc7179f6a6db6d872369e
AyaHaddad <aya.haddad@ynov.com>
2022-03-14 16:49:35 +0100

    add delete + edit button

--- src/components/ListPres.vue
+++ src/components/ListPres.vue
@@ -20,16 +20,49 @@
       </button>
     </div>
     <div class="list-container">
-      <div class="prez-items">
-        <h1>pres</h1>
+      <div v-for="p in pres" :key="p" class="prez-items">
+        <button class="deleteButton">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        <button class="editButton">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-/* const { dialog } = require("electron"); */
+import { ref } from "@vue/reactivity";
 
+/* const { dialog } = require("electron"); */
+const pres = ref([""]);
 function loadPrezInMemory() {}
 
 function uploadPrez() {
