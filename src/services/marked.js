b278e5e9c9aeaded75a51c7ff6e0493fbe5a62e7
AyaHaddad <aya.haddad@ynov.com>
2022-03-27 17:12:40 +0200

    add function replace url for img

--- src/services/marked.js
+++ src/services/marked.js
@@ -1,10 +1,20 @@
 const { marked } = require("marked");
 const fs = require("fs/promises");
+const { getContentFile, checkFilePres } = require("../utils/check");
+
 //requiring path and fs modules
 const path = require("path");
 const os = require("os");
 function markedDownToHtml(data) {
   return marked(data);
+}
+
+function replaceUrlAssetsSlide(content) {
+  //recuper les balise image markdown
+  //parse mardown with regex
+  let imgMD = content.math(/!\[(.*)\]\((.+)\)/g); // array of image in markdown with their url
+  console.log(imgMD);
+  //change url
 }
 
 /**
